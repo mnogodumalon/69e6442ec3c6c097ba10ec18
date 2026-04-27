ddc5ff3172322191bf5f7fbc3e5f487f933dfa3c
Klar <klar@livinglogic.de>
2026-04-27 13:49:09 +0000

    Klar Auto-Deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import '@/lib/sentry';
+import { lazy, Suspense } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import { ActionsProvider } from '@/context/ActionsContext';
 import { ErrorBoundary } from '@/components/ErrorBoundary';
@@ -11,8 +13,15 @@ import ZimmerverwaltungPage from '@/pages/ZimmerverwaltungPage';
 import LeistungsverwaltungPage from '@/pages/LeistungsverwaltungPage';
 import BuchungsverwaltungPage from '@/pages/BuchungsverwaltungPage';
 import GesundheitsprotokollPage from '@/pages/GesundheitsprotokollPage';
+import PublicFormKundenverwaltung from '@/pages/public/PublicForm_Kundenverwaltung';
+import PublicFormKatzenverwaltung from '@/pages/public/PublicForm_Katzenverwaltung';
+import PublicFormZimmerverwaltung from '@/pages/public/PublicForm_Zimmerverwaltung';
+import PublicFormLeistungsverwaltung from '@/pages/public/PublicForm_Leistungsverwaltung';
+import PublicFormBuchungsverwaltung from '@/pages/public/PublicForm_Buchungsverwaltung';
+import PublicFormGesundheitsprotokoll from '@/pages/public/PublicForm_Gesundheitsprotokoll';
+// <public:imports>
+// </public:imports>
 // <custom:imports>
-import { lazy, Suspense } from 'react';
 const NeueBuchungPage = lazy(() => import('@/pages/intents/NeueBuchungPage'));
 const TagesprotokollPage = lazy(() => import('@/pages/intents/TagesprotokollPage'));
 // </custom:imports>
@@ -24,6 +33,14 @@ export default function App() {
         <HashRouter>
           <ActionsProvider>
             <Routes>
+              <Route path="public/69e6442ee20422c9c76e7a9d" element={<PublicFormKundenverwaltung />} />
+              <Route path="public/69e6442f8bf9b400575dae08" element={<PublicFormKatzenverwaltung />} />
+              <Route path="public/69e6442fee4676e450ae217f" element={<PublicFormZimmerverwaltung />} />
+              <Route path="public/69e644305bb831e00fe90d04" element={<PublicFormLeistungsverwaltung />} />
+              <Route path="public/69e644309b0ad64abffd2cc8" element={<PublicFormBuchungsverwaltung />} />
+              <Route path="public/69e644311142a4fbb8d3bc8f" element={<PublicFormGesundheitsprotokoll />} />
+              {/* <public:routes> */}
+              {/* </public:routes> */}
               <Route element={<Layout />}>
                 <Route index element={<DashboardOverview />} />
                 <Route path="kundenverwaltung" element={<KundenverwaltungPage />} />
